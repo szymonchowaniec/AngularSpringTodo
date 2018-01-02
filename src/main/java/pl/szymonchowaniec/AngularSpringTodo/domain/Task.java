2bc110b354634f9cf39cd727a0c5475c44f6872a
Szymon Chowaniec <szymonchowaniec@gmail.com>
2018-01-02 21:16:52 +0100

    add Lombock

--- src/main/java/pl/szymonchowaniec/AngularSpringTodo/domain/Task.java
+++ src/main/java/pl/szymonchowaniec/AngularSpringTodo/domain/Task.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 
 @Entity
 @Data
+@AllArgsConstructor
 public class Task {
     @Id
     @GeneratedValue
@@ -20,77 +21,4 @@ public class Task {
     private LocalDate dueDate;
     private Boolean completed;
 
-    public Task() {
-    }
-
-    public Task(long id, String name, LocalDate dueDate, Boolean completed) {
-        this.id = id;
-        this.name = name;
-        this.dueDate = dueDate;
-        this.completed = completed;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getDueDate() {
-        return dueDate;
-    }
-
-    public void setDueDate(LocalDate dueDate) {
-        this.dueDate = dueDate;
-    }
-
-    public Boolean getCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(Boolean completed) {
-        this.completed = completed;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", dueDate=" + dueDate +
-                ", completed=" + completed +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Task task = (Task) o;
-
-        if (id != task.id) return false;
-        if (name != null ? !name.equals(task.name) : task.name != null) return false;
-        if (dueDate != null ? !dueDate.equals(task.dueDate) : task.dueDate != null) return false;
-        return completed != null ? completed.equals(task.completed) : task.completed == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + (dueDate != null ? dueDate.hashCode() : 0);
-        result = 31 * result + (completed != null ? completed.hashCode() : 0);
-        return result;
-    }
 }
